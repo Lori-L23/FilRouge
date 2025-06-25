079ac6e4235eade0740b31f9bd48fc72d9fcf662
Lori-L23 <atefalori@icloud.com>
2025-06-25 02:48:43 +0100

    Fix Vercel deployment configuration2

--- Innova-front/src/main.jsx
+++ Innova-front/src/main.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from './contexts/AuthContext.jsx'; // Vérifie que le chemin est correct
+import { AuthProvider } from '@/contexts/AuthContext.jsx';
 
 
 

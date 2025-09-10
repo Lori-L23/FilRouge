bf0d43e5d9a33678dfe67861f01239c565773899
Lori <User@Lori.com>
2025-09-10 05:33:25 +0100

    correction chemin authprovider2

--- Innova-front/src/main.jsx
+++ Innova-front/src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './contexts/AuthContext.jsx';
+import { AuthProvider } from './contexts/AuthContext';
 
 
 
